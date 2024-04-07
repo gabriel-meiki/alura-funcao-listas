d368964115fa5f46886d0bb2908257711787e58f
Gabriel Meiki <gabrielmaeda01@gmail.com>
2024-04-07 14:39:25 -0300

    Função com Retorno

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio(); //Guardando o valor aleatório em uma variável
 
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
+// Função com parâmetro, mas não retorna algo
 function mudarTexto(tag, texto){
     corpo = document.querySelector(tag);
     corpo.innerHTML = texto;
-}
+};
 
 mudarTexto('h1', 'Jogo do número secreto');
 mudarTexto('p', 'Escolha um número entre 1 e 10')
 
+// Função sem parâmetro que não retorna algo
 function verificarChute(){
-    console.log('Botão foi clicado')
-}
+    console.log(numeroSecreto)
+};
+
+// Função sem parâmetro que retorna um valor
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+};
