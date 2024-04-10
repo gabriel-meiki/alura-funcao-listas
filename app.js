61af57456eef7b4fad97da0c024888866e591732
Gabriel Meiki <gabrielmaeda01@gmail.com>
2024-04-10 00:35:32 -0300

    Adicionando condições com funções

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ mudarTexto('p', 'Escolha um número entre 1 e 10')
 // Função sem parâmetro que não retorna algo
 function verificarChute(){
     let chute = document.querySelector('input').value; // Pega o valor que é colocado dentro de um input 
-    console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto){
+        mudarTexto('h1', 'Ganhou!!!!');
+        mudarTexto('p', 'Você acertou o número aleatório!');
+    } else {
+        if (chute > numeroSecreto){
+            mudarTexto('p', 'O número secreto é menor.');
+        } else{
+            mudarTexto('p', 'O número secreto é maior.')
+        }
+    }
 };
 
 // Função sem parâmetro que retorna um valor
